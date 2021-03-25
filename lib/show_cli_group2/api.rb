36b952b78405a2d58d2c821c6bb0eba792cdbe4b
cep104 <candicelizabethpeters@gmail.com>
2021-03-25 13:23:25 -0500

    done for the day

--- lib/show_cli_group2/api.rb
+++ lib/show_cli_group2/api.rb
@@ -1,3 +1,14 @@
 class API 
+    def self.get_data
+        response = RestClient.get("http://api.tvmaze.com/search/shows?q=friends")
+        shows = JSON.parse(response)
+        shows.each do |show|
+         new_show  = show["show"]
+        #  attr_accessor :name, :genres, :language, :status
+         Show.new(new_show["name"], new_show["genres"], new_show["language"], new_show["status"])
+        #  binding.pry
+        end
+       binding.pry 
+    end
     # binding.pry
 end
--- lib/show_cli_group2/show.rb
+++ lib/show_cli_group2/show.rb
@@ -1,4 +1,23 @@
 class Show 
+    attr_accessor :name, :genres, :language, :status
+    @@all = []
+    
+    def initialize(name, genres, language, status)
+        @name = name 
+        @genres = genres 
+        @language = language
+        @status = status
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
     #this is a comment
+    #hello there
 #   binding.pry  
 end
